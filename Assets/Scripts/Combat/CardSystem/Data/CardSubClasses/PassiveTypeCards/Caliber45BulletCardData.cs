179ec8d8e41709d4a30d7acbdb03d4568a3fa551
adriamiralozano <90398377+adriamiralozano@users.noreply.github.com>
2025-06-07 14:28:05 +0200

    balaScriptableUpdate

--- Assets/Scripts/Combat/CardSystem/Data/CardSubClasses/PassiveTypeCards/Caliber45BulletCardData.cs
+++ Assets/Scripts/Combat/CardSystem/Data/CardSubClasses/PassiveTypeCards/Caliber45BulletCardData.cs
@@ -1,13 +1,14 @@
 // Caliber45BulletCardData.cs
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "NewCaliber45BulletCard", menuName = "Card System/Caliber 45 Bullet Card Data")]
-public class Caliber45BulletCardData : PassiveCardData //hereda de PassiveCardData
+[CreateAssetMenu(fileName = "Caliber45BulletCardData", menuName = "Card System/Cards/Caliber 45 Bullet")]
+public class Caliber45BulletCardData : CardData 
 {
     public string bulletCardID = "Caliber45Bullet"; // Para identificarla fácilmente en el código
+    private void OnEnable()
+    {
+        cardID = "Caliber45Bullet";
+        type = CardType.Passive;
+    }
 
-    // No necesitamos sobrescribir ExecuteEffect() aquí de nuevo,
-    // ya que PassiveCardData ya tiene una implementación (aunque sea vacía/de debug).
-    // Si la bala tuviera un efecto *específico* al ser "jugada" como pasiva,
-    // entonces sí lo sobrescribiríamos aquí.
 }
